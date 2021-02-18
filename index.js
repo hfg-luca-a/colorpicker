07a7860d6700b462a30eb5ced55f5225f78cd0c8
hfg-luca-a <72493170+hfg-luca-a@users.noreply.github.com>
2021-02-18 10:54:38 +0100

    ++

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+//Startup Webserver
 const express = require('express');
 const app = express();
 app.listen(4000, () => console.log('Server UP! \nhöre auf Port 4000'));
 app.use(express.static('public'));
 app.use(express.json());
+
+
+//Verbindung zu Mqtt Broker
+let mqtt = require('mqtt')
+let client = mqtt.connect('mqtt:/192.168.0.132')
+
+
+
+function publishData(data) {
+    console.log(data[2])
+    console.log(data[0])
+    client.publish('/led/helligkeit', data[2])
+    client.publish('/led/farbe', data[0])
+}
+
+
+app.post('/hsvdata', function (req, res) {
+    const data = req.body
+    publishData(data)
+});
